20666040d49d4c1561239c422e350d1acf355a47
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-01-21 18:33:26 +0000

    Added corrections to checkout form data in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -18,15 +18,15 @@ def cache_checkout_data(request):
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('bag')),
+            'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later')
-    return HttpResponse(content=e, status=400)
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -22,7 +22,9 @@ def webhook(request):
     event = None
 
     try:
-        event = stripe.Webhook.construct_event(payload, sig_header, wh_secret)
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, wh_secret
+        )
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
@@ -32,19 +34,17 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    #Set up a webhook handler
+    # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
-    #Map webhook events to relevant handler functions
+    # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
         'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
-    #Get the webhook type from Stripe
+    # Get the webhook type from Stripe
     event_type = event['type']
-
-    event_handler = event_map.get(event_type, handler.handle_event)
 
     # If there's a handler for it, get it from the event map
     # Use the generic one by default

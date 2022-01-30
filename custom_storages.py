afac75384cc9fe84943e4911b56c7713027cf94a
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2022-01-30 16:20:39 +0000

    Amend file errors for Heroku Build pt3

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storages
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    loacation = settings.STATICFILES_LOCATION
+    location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
-    loacation = settings.MEDIAFILES_LOCATION
+    location = settings.MEDIAFILES_LOCATION
